3803a4258164c5b50ccbe83232af96000e164961
Florian Bernard <florian.bernard.0@gmail.com>
2020-04-25 19:15:00 +0200

    add scroll before swipe

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 
 import SEO from '../components/seo'
 import { getProductUrl } from '../util/link'
@@ -16,55 +16,61 @@ import styles from './index.module.less'
 const IndexPage = ({ data, location }) => {
   const firstProduct =
     data.allStripeProduct.edges.length && data.allStripeProduct.edges[0].node
-  return (
-    <>
-      <SEO lang="fr" location={location} />
-      <SwipeSpring>
-        {() => (
+
+  const content = useMemo(
+    () => (
+      <>
+        <p>
+          <MakramondeBijouImage />
+        </p>
+        <h1>
+          Des bijoux artisanaux aux influences ethniques et antiques d'autour du
+          monde
+        </h1>
+        <p>
+          Tous les bijoux sont fabriqués entièrement à la main dans mon atelier
+          en Haute-Savoie, à Annecy. <br />
+          Les matériaux utilisés sont de qualités ; argent 925 poinçonné de mon
+          poinçon de maître, laiton et cuivre pour les autres métaux (exempts de
+          nickel), fil ciré résistant à l'eau pour le macramé.
+        </p>
+        <p>
+          Livraison à 1 centime à partir de{' '}
+          {SHIPPING[FRANCE_METRO].discountFrom / 100}€, en France Metropolitaine
+          uniquement.
+        </p>
+        {!!firstProduct && (
           <>
-            <p>
-              <MakramondeBijouImage />
-            </p>
-            <h1>
-              Des bijoux artisanaux aux influences ethniques et antiques
-              d'autour du monde
-            </h1>
-            <p>
-              Tous les bijoux sont fabriqués entièrement à la main dans mon
-              atelier en Haute-Savoie, à Annecy. <br />
-              Les matériaux utilisés sont de qualités ; argent 925 poinçonné de
-              mon poinçon de maître, laiton et cuivre pour les autres métaux
-              (exempts de nickel), fil ciré résistant à l'eau pour le macramé.
-            </p>
-            <p>
-              Livraison à 1 centime à partir de{' '}
-              {SHIPPING[FRANCE_METRO].discountFrom / 100}€, en France
-              Metropolitaine uniquement.
-            </p>
-            {!!firstProduct && (
-              <>
-                <p>Pour voir mes dernières créations, c'est par ici :</p>
-                <p className={styles.explore}>
-                  <SwipeLink direction="left" to={getProductUrl(firstProduct)}>
-                    <Button type="primary" size="large">
-                      Explorer
-                    </Button>
-                  </SwipeLink>
-                </p>
-              </>
-            )}
-            {!firstProduct && (
-              <p>
-                Je n'ai actuellement aucune création à vous proposer en ligne.
-                Revenez bientôt pour voir mes nouveautés !
-              </p>
-            )}
-            <p>
-              <AssemblageImage />
+            <p>Pour voir mes dernières créations, c'est par ici :</p>
+            <p className={styles.explore}>
+              <SwipeLink direction="left" to={getProductUrl(firstProduct)}>
+                <Button type="primary" size="large">
+                  Explorer
+                </Button>
+              </SwipeLink>
             </p>
           </>
         )}
-      </SwipeSpring>
+        {!firstProduct && (
+          <p>
+            Je n'ai actuellement aucune création à vous proposer en ligne.
+            Revenez bientôt pour voir mes nouveautés !
+          </p>
+        )}
+        <p>
+          <AssemblageImage />
+        </p>
+      </>
+    ),
+    [firstProduct]
+  )
+  const swipe = useMemo(() => <SwipeSpring>{() => content}</SwipeSpring>, [
+    firstProduct
+  ])
+  return (
+    <>
+      <SEO lang="fr" location={location} />
+      {swipe}
     </>
   )
 }
