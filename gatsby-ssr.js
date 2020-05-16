015fef1ac08a872dbc55c571f8a55e9348cb9ecb
Florian Bernard <florian.bernard.0@gmail.com>
2020-05-16 18:46:16 +0200

    prioritize data helmet tags

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,27 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+// Hack, to reorder the helmet components as first in <head> tag
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  /**
+   * @type {any[]} headComponents
+   */
+  const headComponents = getHeadComponents()
+  headComponents.sort((a, b) => {
+    const aIsDataHelmet = isDataHelmet(a)
+    const bIsDataHelmet = isDataHelmet(b)
+    if (aIsDataHelmet && bIsDataHelmet) {
+      return 0
+    }
+    if (aIsDataHelmet) {
+      return -1
+    }
+    if (bIsDataHelmet) {
+      return 1
+    }
+    return 0
+  })
+  replaceHeadComponents(headComponents)
+}
+
+const isDataHelmet = x => x.props && x.props['data-react-helmet']
